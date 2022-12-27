c758098b5b9c7389e7fbaf9196994b466cbb3a03
dudgus257 <111178186+dudgus257@users.noreply.github.com>
2022-12-27 10:08:50 +0900

    Add files via upload

---
+++ 221227 김영현/custom.js
@@ -0,0 +1,31 @@
+$(function(){
+    // Slick Slider - Welcome
+    $('.slideshow').slick({
+        infinite: true, // 무한반복
+        dots: false, // 동그라미 네비게이션 사용안함
+        autoplay: true, // 자동 재생
+        autoplaySpeed: 2000, // 자동 재생 시 머무는 시간
+        fade: true, // 크로스 페이드 트랜지션
+        speed: 1000, // 자동 재생 트랜지션 시간
+        pauseOnHover: false // 마우스 오버될 때 멈춤 사용안함
+    });
+
+    // TypeIt - Welcome
+    $('#typing').typeIt({
+        strings: ["home workout", "cardio & weight", "with HealthCare!"], // 타이핑 텍스트 입력
+        speed: 100, // 알파벳 타이핑 속도
+        autoStart: true, // 자동 재생 사용
+        breakLines: false, // 줄 바꿈 사용안함
+    });
+})
+
+// faq-Accordion
+$(function(){
+    $('.faq-desc').eq(0).show()
+    $('.faq-title').click(function(){
+        $(this).next().stop().slideDown()
+        $(this).parent().siblings().children('.faq-desc').stop().slideUp()
+        $(this).parent().addClass('active')
+        $(this).parent().siblings().removeClass('active')
+    })
+})
